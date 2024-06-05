517421a6fbb4d8df2b16fda0560f8478765de83c
Rockyibarra96 <rockyibarralutz@gmail.com>
2024-06-05 12:08:55 -0700

    app working

--- react-rainbow/src/App.js
+++ react-rainbow/src/App.js
@@ -1,17 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 import React, { useState } from 'react'
 
 import ColorBlock from './ColorBlock'
 
+import ColorForm from './ColorForm'
+
 function App(){
-    let colors = [
-        'violet', 'blue',
-        'lightblue', 'green',
-        'greenyellow', 'yellow',
-        'orange', 'red'
-    ]
+    let [colors, setColors] = useState ([
+      'violet', 'blue',
+      'lightblue', 'green',
+      'greenyellow', 'yellow',
+      'orange', 'red'
+  ])
     
     let colorMap = colors.map((color, i) => {
         return (
@@ -19,11 +21,16 @@ function App(){
         )
     })
 
+    const addColor = (newColor) => {
+      setColors([...colors, newColor])
+    }
+
     return (
       <div className="App">
         {colors.map((color, i) => 
             <ColorBlock key={i} color={color} />
         )}
+        <ColorForm addColor={addColor} />
       </div>
   
     )
